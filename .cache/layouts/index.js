25f18086d66ca57e712637e98147ee0a4636d954
mo-ro <gainmoro@gmail.com>
2018-08-18 01:05:21 +0900

    add library

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/moro/develop/moai-folio/src/layouts/index.js"
+  import data from "/Users/moro/develop/moai-folio/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,23 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/moro/develop/moai-folio/.cache/layouts/index.js"))
+}
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/moro/develop/moai-folio/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/moro/develop/moai-folio/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/moro/develop/moai-folio/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/moro/develop/moai-folio/src/pages/page-2.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/moro/develop/moai-folio/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/moro/develop/moai-folio/.cache/json/dev-404-page.json"),
+  "404.json": require("/Users/moro/develop/moai-folio/.cache/json/404.json"),
+  "index.json": require("/Users/moro/develop/moai-folio/.cache/json/index.json"),
+  "page-2.json": require("/Users/moro/develop/moai-folio/.cache/json/page-2.json"),
+  "404-html.json": require("/Users/moro/develop/moai-folio/.cache/json/404-html.json")
+}
